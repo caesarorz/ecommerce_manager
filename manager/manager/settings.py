3d9521bba9d32c7d2d177cfeb6ad779262ad1669
Cesar Orozco <caesar.orz@gmail.com>
2023-05-24 14:41:41 -0600

    Cors and aws for s3 bucket

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -172,3 +172,12 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+### AWS S3
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY', '')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME', '')
