ff09ebdb6693c8289ae7309f991b87854dec284d
Cesar Orozco <caesar.orz@gmail.com>
2023-07-12 11:21:01 -0600

    Set last enviroment variables, disable admin for now

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'local_secret_key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS.extend(
--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('products.urls')),
     path('api/', include('orders.urls')),
