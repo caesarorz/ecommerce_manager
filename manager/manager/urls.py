5d4b58624596d0cf05cd31100f87785434e1e951
Cesar Orozco <caesar.orz@gmail.com>
2023-09-04 09:57:30 -0600

    Temporary enable admin

--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-
+from django.contrib import admin
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -7,6 +7,7 @@ from drf_spectacular.views import (
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema')),
     path('api/', include('users.urls')),
